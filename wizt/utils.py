98d4ae4c3d1abc842808bcc2b877a1ed5c55872b
Ambition <it.ninja.19881110@gmail.com>
2019-06-07 15:07:44 +0900

    handle exception on boto3

--- wizt/utils.py
+++ wizt/utils.py
@@ -1,6 +1,7 @@
 import random
 import string
 import boto3
+from botocore.exceptions import ClientError
 #conf for boto3 for AWS
 # Yuo need to have AWS credentails in ~/.aws/credentials
 # [default]
@@ -13,33 +14,44 @@ def randomString(stringLength=10):
 
 
 def upload_file(file):
-    s3 = boto3.resource('s3')
-    s3_bucket_name = 'pit'
-    s3.Bucket(s3_bucket_name).put_object(Key='test',Body=file)
-    bucket_location = boto3.client('s3').get_bucket_location(Bucket=s3_bucket_name)
-    object_url = "https://s3-{0}.amazonaws.com/{1}/{2}".format(
-        bucket_location['LocationConstraint'],
-        s3_bucket_name,
-        key_name)
+    try:
+        s3 = boto3.resource('s3')
+        s3_bucket_name = 'pit'
+        s3.Bucket(s3_bucket_name).put_object(Key='test',Body=file)
+        bucket_location = boto3.client('s3').get_bucket_location(Bucket=s3_bucket_name)
+        object_url = "https://s3-{0}.amazonaws.com/{1}/{2}".format(
+            bucket_location['LocationConstraint'],
+            s3_bucket_name,
+            key_name)
+    except Exception as e:
+        print("Exception issued on upload_file \n",e)
+
 
 
 def send_push_notification(target_arn, subject, message):
-    client = boto3.client('sns', region_name='ap-southeast-1')
-    # target_arn = "arn:aws:sns:ap-southeast-1:417479686763:endpoint/GCM/wizt-new/1a298313-61dd-3e04-9b33-99f0cfa90210"
-    response = client.publish(
-        TargetArn=target_arn,  # Target Amazon Resource Name
-        Message=message,
-        Subject=subject,
-        MessageStructure='string'
-    )
+    try:
+        client = boto3.client('sns', region_name='ap-southeast-1')
+        # target_arn = "arn:aws:sns:ap-southeast-1:417479686763:endpoint/GCM/wizt-new/1a298313-61dd-3e04-9b33-99f0cfa90210"
+        response = client.publish(
+            TargetArn=target_arn,  # Target Amazon Resource Name
+            Message=message,
+            Subject=subject,
+            MessageStructure='string'
+        )
+    except Exception as e:
+        print("Exception issued on push notification \n",e)
+
 
 
 def send_broadcast_notification(topic_arn, subject, message):
-    client = boto3.client('sns', region_name='ap-southeast-1')
-    # target_arn = "arn:aws:sns:ap-southeast-1:417479686763:endpoint/GCM/wizt-new/1a298313-61dd-3e04-9b33-99f0cfa90210"
-    response = client.publish(
-        TopicArn=topic_arn,  # Target Amazon Resource Name
-        Message=message,
-        Subject=subject,
-        MessageStructure='string'
-    )
+    try:
+        client = boto3.client('sns', region_name='ap-southeast-1')
+        # target_arn = "arn:aws:sns:ap-southeast-1:417479686763:endpoint/GCM/wizt-new/1a298313-61dd-3e04-9b33-99f0cfa90210"
+        response = client.publish(
+            TopicArn=topic_arn,  # Target Amazon Resource Name
+            Message=message,
+            Subject=subject,
+            MessageStructure='string'
+        )
+    except Exception as e:
+        print("Exception issued on broadcast notification \n",e)
